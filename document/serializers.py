85df1ac1fb1e727a1112335d6e0d84875b17e398
root <root@anshay>
2024-11-30 00:45:59 +0530

    Added custom validator in the serializer for validating the input data received from the api

--- document/serializers.py
+++ document/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import UploadedDocument
+from .api_exceptions import FileNotUploadedException
+
 
 class UploadedDocumentSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        if 'file_path' not in data.keys() or data['file_path'] is None:
+            raise FileNotUploadedException()
+
+        return data
+
     class Meta:
         model=UploadedDocument
         fields=['id', 'file_name', 'file_path', 'date_uploaded']
